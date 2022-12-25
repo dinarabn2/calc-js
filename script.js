500c02e032a4ebd65494fd50aabc2c9778cc70bf
dinarabn2 <107791872+dinarabn2@users.noreply.github.com>
2022-12-25 23:29:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const calc = document.querySelector('.calc');
+const result = document.querySelector('#result');
+
+calc.addEventListener('click', (event) => {
+    if (!event.target.classList.contains('calc__btn')) return;
+
+    const value = event.target.innerText;
+
+    switch(value) {
+        case "C" :
+            result.innerText = "";
+            break;
+
+        case "=" :
+            result.innerText = eval(result.innerText).toFixed(2);
+            break;
+
+        default :
+            result.innerText += value;
+    }
+});
